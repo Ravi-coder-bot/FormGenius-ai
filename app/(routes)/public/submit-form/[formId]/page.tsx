2ca137446367fcf9c2e312f41ae5a52f2cad239e
ravinder <ravibeniwal931@gmail.com>
2025-01-11 08:47:18 +0530

    share-btn-changes

---
+++ app/(routes)/public/submit-form/[formId]/page.tsx
@@ -0,0 +1,21 @@
+import { fetchPublishFormById } from "@/actions/form.action";
+import React from "react";
+import { FormBlockInstance } from "@/@types/form-block.type";
+import NotAvaliable from "../../_components/NotAvaliable";
+import FormSubmitComponent from "../../_components/FormSubmitComponent";
+
+
+const Page = async ({ params }: { params: { formId: string } }) => {
+  const { formId } = params;
+
+  const { form } = await fetchPublishFormById(formId);
+
+  if (!form) {
+    return <NotAvaliable />;
+  }
+
+  const blocks = JSON.parse(form.jsonBlocks) as FormBlockInstance[];
+  return <FormSubmitComponent formId={formId} blocks={blocks} />;
+};
+
+export default Page;
